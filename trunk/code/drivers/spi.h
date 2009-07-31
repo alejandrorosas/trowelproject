1774cbfb0dc3b72a2912f136f33561f90ac7d265
clement.bdr <clement.bdr@16d9f100-46de-11de-89ec-3d9467717a5a>
2009-07-31 10:49:58 +0000

    reorganized folders

--- trunk/code/drivers/spi.h
+++ trunk/code/drivers/spi.h
@@ -4,11 +4,13 @@
 extern uint8_t spi_last_read;
 
 void spi_init(void);
-void spi_select(int on);
+void spi_radio_select(int on);
 
 int spi_write(const uint8_t* data, int len, int (*done)(void));
 void spi_write_single(uint8_t data);
 int spi_read(uint8_t* data, int len, int (*done)(void));
 uint8_t spi_read_single(void);
+
+uint8_t spi_read_somi(void);
 
 #endif
