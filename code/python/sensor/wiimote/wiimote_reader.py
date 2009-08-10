1e8099463e0623c91c19eb1279029105499ff62f
arthur.vuillard <arthur.vuillard@16d9f100-46de-11de-89ec-3d9467717a5a>
2009-08-10 21:39:21 +0000

    Added wiimote_oscillo and csv_oscillo

--- code/python/sensor/wiimote/wiimote_reader.py
+++ code/python/sensor/wiimote/wiimote_reader.py
@@ -1,20 +1,43 @@
-from sensor import ScopeReader
+##from sensor import ScopeReader
+
+import oscillo
 
 from sensor.wiimote import *
 
 import time
+import numpy
 
-class WiimoteReader(ScopeReader):
+class WiimoteReader(oscillo.SensorReader):
     def __init__(self):
         self.wiimote = giveMeAWiimote()
         self.wiimote.set_msg_callback(self.callback)
-        self.time_init = time.time()
-        self.data = [None,None,None,None]
 
-    def refresh(self):
-        return self.data
+        self.time_init = time.time()
+
+        self.init_cache()
+
+    def init_cache(self):
+        self.cache_t=[]
+        self.cache_x=[]
+        self.cache_y=[]
+        self.cache_z=[]
+
+    def get_new_data(self):
+        data = {'t':numpy.array(self.cache_t),
+                'x':numpy.array(self.cache_x),
+                'y':numpy.array(self.cache_y),
+                'z':numpy.array(self.cache_z)}
+
+        self.init_cache()
+        return data
 
     def callback(self,msgs):
         for msg in msgs:
             if msg[0]==cwiid.MESG_ACC:
-                self.data= [time.time()-self.time_init,msg[1][0],msg[1][1],msg[1][2]]
+                self.cache_t.append(time.time()-self.time_init)
+                self.cache_x.append(msg[1][0]-128)
+                self.cache_y.append(msg[1][1]-128)
+                self.cache_z.append(msg[1][2]-128)
+
+    def terminate(self):
+        self.wiimote.close()
