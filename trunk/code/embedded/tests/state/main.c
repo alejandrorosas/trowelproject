61a3e4563c2238825683354d6c0544482f815af6
clement.bdr <clement.bdr@16d9f100-46de-11de-89ec-3d9467717a5a>
2010-05-30 21:15:05 +0000

    updated state example with timer

--- trunk/code/embedded/tests/state/main.c
+++ trunk/code/embedded/tests/state/main.c
@@ -1,36 +1,34 @@
 #include <io.h>
+#include <signal.h>
 #include "clock.h"
 #include "leds.h"
 #include "state.h"
 #include "event.h"
+#include "timer.h"
 
 volatile unsigned int i;                    // volatile to prevent optimization
 
 int main(void) {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+    eint();
 
     clock_dco_set(1); // DCO 1MHz
     clock_mclk_set(CLOCK_SOURCE_DCO, 8); // MCLK 125kHz
     clock_smclk_set(CLOCK_SOURCE_DCO, 4);
-    clock_aclk_set(1);
+    clock_aclk_set(1); // 12 kHz
 
     leds_init();
-    leds_on(LEDS_ALL);
+
+	timer_init();
+	timer_start(ACLK, 1);
 
 	event_init();
 	st_init();
 
-	i = 65535;
-	do
-		(i--);
-	while (i != 0);
-	
 	first_init();
 
-	
 	while (1) {
 		event_process();
-
 	}
     
     return 0;
