6b3eafe3174ec00e7201c4db6a5f7d7c8bcdbb8a
clement.bdr <clement.bdr@16d9f100-46de-11de-89ec-3d9467717a5a>
2010-05-30 17:06:08 +0000

    *debugged state, first example working with LEDs

--- trunk/code/embedded/sys/state.c
+++ trunk/code/embedded/sys/state.c
@@ -1,4 +1,5 @@
 #include <io.h>
+#include "event.h"
 #include "state.h"
 
 // Private Structures
@@ -8,7 +9,7 @@ struct st_event_data {
 };
 
 // Private Function Prototypes
-static void exec_state(st_state* state, st_event* event);
+static void exec_state(struct st_state *state, struct st_event *event);
 static void exec_func(void* data);
 static struct st_event_data* get_available_dataslot(void);
 
@@ -29,15 +30,11 @@ void st_init(void) {
 }
 
 void st_init_chain(struct st_chain* chain) {
-	int16_t i;
 	chain->first_evt = 0x0;
 	chain->first_state = 0x0;
 	chain->first_transition = 0x0;
 
 	chain->active_state = 0x0;
-	for (i=0;i<ST_CHAIN_WAITING_EVENTS_MAX) {
-		chain->waiting_events[i] = 0x0;
-	}
 }
 
 void st_add_event(struct st_chain *chain, struct st_event *evt) {
@@ -51,7 +48,7 @@ void st_add_event(struct st_chain *chain, struct st_event *evt) {
 	}
 }
 
-void st_add_state(struct st_chain *chain, struct st_event *st) {
+void st_add_state(struct st_chain *chain, struct st_state *st) {
 	
 	if (chain->first_state == 0x0) {
 		chain->first_state = st;
@@ -68,12 +65,12 @@ void st_add_transition(struct st_chain *chain, struct st_transition *transition)
 		chain->first_transition = transition;
 		transition->next = 0x0;
 	} else {
-		st->next = chain->first_transition;
-		chain->first_state = transition;
+		transition->next = chain->first_transition;
+		chain->first_transition = transition;
 	}
 }
 
-struct void st_notify_event(struct st_chain *chain, struct st_event *evt) {
+void st_notify_event(struct st_chain *chain, struct st_event *evt) {
 	struct st_state *active;
 	struct st_transition *transition;
 
@@ -86,13 +83,15 @@ struct void st_notify_event(struct st_chain *chain, struct st_event *evt) {
 	}
 	
 	// loop over the transitions of this chain to find one mathing
-	for (transition = chain->first_transition; transition = transition->next; transition != 0x0) {
+	for (transition = chain->first_transition;
+			transition != 0x0;
+			transition = transition->next) {
 		if ( ( (transition->from_state == active) ||
 					(transition->from_state == 0x0) ) &&
 				(transition->event == evt) ) {
 			// we found the transition!
 			// call the state
-			exec_state(transition->to_state, evt)
+			exec_state(transition->to_state, evt);
 			// update the active state pointer
 			chain->active_state = transition->to_state;
 			return;
@@ -100,12 +99,12 @@ struct void st_notify_event(struct st_chain *chain, struct st_event *evt) {
 	}
 }
 
-struct void st_set_active_state(struct st_chain* chain, struct st_state *state) {
+void st_set_active_state(struct st_chain* chain, struct st_state *state) {
 	
 	// call the state
-	exec_state(state, 0x0)
+	exec_state(state, 0x0);
 	// update the active state pointer
-	chain->active_state = transition->to_state;
+	chain->active_state = state;
 }
 
 static struct st_event_data* get_available_dataslot(void) {
@@ -113,7 +112,7 @@ static struct st_event_data* get_available_dataslot(void) {
 
 	for (i = 0; i < ST_EVENT_QUEUE_LENGTH; i++) {
 		ix = (i + event_data_queue_search_start) % ST_EVENT_QUEUE_LENGTH;
-		if (event_data_queue[ix] == 0x0) {
+		if (event_data_queue[ix].func == 0x0) {
 			// found a place!
 			// update search queue
 			event_data_queue_search_start += 1;
@@ -125,15 +124,20 @@ static struct st_event_data* get_available_dataslot(void) {
 	return 0x0;
 }
 
-static void exec_state(st_state* state, st_event* event) {
+static void exec_state(struct st_state* state, struct st_event* event) {
 	struct st_event_data *data;
 
+	// get a data slot
 	data = get_available_dataslot();
 	if (data == 0x0)
 		return;
 
-	data->func = state->exec_func;
-	data->data = event; 
+	// store data in it
+	data->func = state->exec;
+	data->data = event;
+
+	// post state execution
+	event_post(exec_func, data);
 }
 
 
@@ -145,9 +149,9 @@ static void exec_func(void* data) {
 	event_data = (struct st_event_data *) data;
 
 	// call the function
-	(data->func)(data->data);
+	(event_data->func)(event_data->data);
 
 	// clear the data slot
-	data->func = 0x0;
-	data->data = 0x0;
+	event_data->func = 0x0;
+	event_data->data = 0x0;
 }
