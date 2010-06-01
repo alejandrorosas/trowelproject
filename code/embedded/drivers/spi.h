10573cdfc290dbcafea68fb82dc0b57ca8f6cc8f
clement.bdr <clement.bdr@16d9f100-46de-11de-89ec-3d9467717a5a>
2010-06-01 22:17:43 +0000

    started moving usci transfer to interrupt driven

--- code/embedded/drivers/spi.h
+++ code/embedded/drivers/spi.h
@@ -1,43 +1,22 @@
 #ifndef _SPI_H_
 #define _SPI_H_
 
-#define SPI_RADIO 0x1
-#define SPI_ACCEL 0x2
+typedef int (*spi_transfer_done_cb_t)(); 
 
 extern uint8_t spi_last_read;
-extern int spi_last_config;
 
 void spi_init(void);
-#define spi_radio_select()    do { \
-  spi_accel_deselect(); \
-  if (spi_last_config != SPI_RADIO) { \
-    spi_last_config = SPI_RADIO; \
-    UCB0CTL1 = UCSSEL_SMCLK | UCSWRST; \
-    UCB0CTL0 = UCCKPH | UCMSB | UCMST | UCSYNC; \
-    UCB0CTL1 &= ~UCSWRST; \
-  } \
-  P3OUT &= ~1; \
-} while (0)
-
-#define spi_radio_deselect()  P3OUT |= 1
-#define spi_accel_select()    do { \
-  spi_radio_deselect(); \
-  if (spi_last_config != SPI_ACCEL) { \
-    spi_last_config = SPI_ACCEL; \
-    UCB0CTL1 = UCSSEL_SMCLK | UCSWRST; \
-    UCB0CTL0 = UCCKPL | UCMSB | UCMST | UCSYNC; \
-    UCB0CTL1 &= ~UCSWRST; \
-  } \
-  P4OUT &= ~BV(6); \
-} while (0)
-
-#define spi_accel_deselect()  P4OUT |= BV(6)
+void spi_select_radio();
+void spi_deselect_radio();
 
 int spi_write(const uint8_t* data, int len);
 void spi_write_single(uint8_t data);
+int spi_write_interrupt(const uint8_t* data, int len, spi_transfer_done_cb_t cb);
+
 int spi_read(uint8_t* data, int len);
 uint8_t spi_read_single(void);
+int spi_read_interrupt(uint8_t* data, int len, spi_transfer_done_cb_t cb);
 
-#define spi_read_somi() P3IN & (1<<2)
+#define spi_read_somi() (P3IN & BV(2)))
 
 #endif
